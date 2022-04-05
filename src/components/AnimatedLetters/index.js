eef942c30161a09c55e809e71ff5bae87833fcb4
brian shin <bshin132@gmail.com>
2022-04-05 13:35:13 -0700

    remove unnecessary classNames and setup AnimatedLetters comp

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import './index.scss';
+
+const Container = styled.div``;
+
+
+
+export default function AnimatedLetters() {
+  return (
+    <Container>
+
+    </Container>
+  );
+}
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,45 +1,69 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../../assets/images/logo.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
 
-const Container = styled.div`
-`;
+const Container = styled.div``;
 
-const TextCont = styled.div``;
+const TextCont = styled.div`
+  position: absolute;
+  left: 10%;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 40%;
+  max-height: 90%;
+`;
 
 const Intro = styled.h1`
-  color:#ffffff;
-  font-size:53px;
-  margin:0;
-  font-family:'Coolvetica';
-  font-weight:400;
+  color: #ffffff;
+  font-size: 53px;
+  margin: 0;
+  font-family: 'Coolvetica';
+  font-weight: 400;
+  &::before {
+    content: '<h1>';
+    font-family: 'La Belle Aurore';
+    color: #ffd700;
+    font-size: 18px;
+    position: absolute;
+    margin-top: -40px;
+    left: 15px;
+    opacity: 0.6;
+  }
+
+  &::after {
+    content: '<h1>';
+    font-family: 'La Belle Aurore';
+    color: #ffd700;
+    font-size: 18px;
+    position: absolute;
+    margin-top: 18px;
+    margin-left: 20px;
+    animation: fadeIn 1s 1.7s backwards;
+    opacity: 0.6;
+  }
 `;
 
-
-
 const Subheader = styled.h2`
-  color:#8d8d8d;
-  margin-top:20px;
-  font-weight:400;
-  font-size:11px;
-  font-family:sans-serif;
-  letter-spacing:3px;
-  animation:fadeIn 1s 1.8s backwards;
+  color: #8d8d8d;
+  margin-top: 20px;
+  font-weight: 400;
+  font-size: 11px;
+  font-family: sans-serif;
+  letter-spacing: 3px;
+  animation: fadeIn 1s 1.8s backwards;
 `;
 
 export default function Home() {
   return (
-    <Container className='container homepage'>
-      <TextCont className='text'>
+    <Container className="container">
+      <TextCont>
         <Intro>
-          Hi, <br /> I'm
-          Brian
+          Hi, <br /> I'm Brian
           <br />
           web developer
         </Intro>
-        <Subheader>Frontend Developer</Subheader>
+        <Subheader>Frontend Developer / UI/UX Designer</Subheader>
         <Link to="/contact" className="button">
           CONTACT ME
         </Link>
