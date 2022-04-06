748471d2c8e8ecad86717934e836f774ae2522a7
brian shin <bshin132@gmail.com>
2022-04-05 17:25:13 -0700

    setup contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import './index.scss';
+
+export default function Contact() {
+  return (
+    <>
+    </>
+  );
+}
