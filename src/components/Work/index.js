77e52bfebbb0a7d785450fd1dfdc1e600a04fa24
bshin132 <bshin132@gmail.com>
2022-04-19 17:01:05 -0700

    change logo and title, start work page but not completed

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -3,5 +3,28 @@ import './index.scss';
 import styled from 'styled-components';
 
 export default function Work() {
-  return (<></>);
+  return (
+    <div id="page" class="en">
+      <div id="demo" class="container gallery magicwall">
+        <ul class="magicwall-grid">
+          Project Name
+          <li data-thumb="assets/images/portfolio/1-port-thumb.png">
+            <div class="hover-content vcenter"></div>
+            <a
+              href="assets/images/portfolio/1-port-1.png"
+              title='<a target="_blank">188Bet</a>'
+              class="colorbox"
+              rel="magicwall1"
+            ></a>
+            <a
+              href="assets/images/portfolio/1-port-devices.png"
+              title='<a target="_blank">188Bet</a>'
+              class="colorbox"
+              rel="magicwall1"
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
