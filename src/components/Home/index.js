9220a06f8f0f45decb5bea54e67995ec0cba7c3f
bshin132 <bshin132@gmail.com>
2022-04-23 15:11:12 -0700

    restyle homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,67 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import './index.scss';
 import Logo from '../Logo';
 import AnimatedLetters from '../AnimatedLetters';
 import Loader from 'react-loaders';
-
-const Container = styled.div``;
-
-const TextCont = styled.div`
-  position: absolute;
-  left: 10%;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 40%;
-  max-height: 90%;
-`;
-
-const Intro = styled.h1`
-  color: #ffffff;
-  font-size: 53px;
-  margin: 0;
-  font-family: 'Coolvetica';
-  font-weight: 400;
-  &::before {
-    content: '<h1>';
-    font-family: 'La Belle Aurore';
-    color: #ffd700;
-    font-size: 18px;
-    position: absolute;
-    margin-top: -40px;
-    left: 15px;
-    opacity: 0.6;
-  }
-
-  &::after {
-    content: '</h1>';
-    font-family: 'La Belle Aurore';
-    color: #ffd700;
-    font-size: 18px;
-    position: absolute;
-    margin-top: 18px;
-    margin-left: 20px;
-    animation: fadeIn 1s 1.7s backwards;
-    opacity: 0.6;
-  }
-`;
-
-const Subheader = styled.h2`
-  color: #8d8d8d;
-  margin-top: 20px;
-  font-weight: 400;
-  font-size: 14px;
-  font-family: sans-serif;
-  letter-spacing: 3px;
-  animation: fadeIn 1s 1.8s backwards;
-`;
-
-const LogoContainer = styled.div`
-  position: relative;
-  animation: fadeIn 1s 1.8s backwards;
-  top: 500px;
-`;
 
 export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -95,9 +37,9 @@ export default function Home() {
 
   return (
     <>
-      <Container className="container">
-        <TextCont>
-          <Intro>
+      <div className="container home-page">
+        <div className="text-home">
+          <h1>
             <span className={letterClass}>H</span>
             <span className={`${letterClass} _12`}>i,</span>
             <br />
@@ -114,16 +56,16 @@ export default function Home() {
               array={jobArray}
               index={20}
             />
-          </Intro>
-          <Subheader>Frontend Developer / UI/UX Designer</Subheader>
+          </h1>
+          <h2>Frontend Developer / UI/UX Designer</h2>
           <Link to="/contact" className="button">
             CONTACT ME
           </Link>
-        </TextCont>
-        <LogoContainer>
+        </div>
+        <div>
           <Logo />
-        </LogoContainer>
-      </Container>
+        </div>
+      </div>
       <Loader type="pacman" />
     </>
   );
